f633416571d88ec378d11bb2899d56a3555f6e93
Omatple <omatple@gmail.com>
2024-11-12 13:08:34 +0100

    Creo las tablas de la base de datos y organizo la estructura de carpetas del proyecto

---
+++ sql/tables.sql
@@ -0,0 +1,42 @@
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20) UNIQUE NOT NULL
+);
+
+CREATE TABLE users (
+    username VARCHAR(16) PRIMARY KEY NOT NULL CHECK (username NOT LIKE '% %'),
+    email VARCHAR(320) UNIQUE NOT NULL CHECK (email LIKE '%@%.%'),
+    password VARCHAR(60) NOT NULL,
+    img VARCHAR(255) NOT NULL DEFAULT 'img/default.png',
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE chats (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username1 VARCHAR(16) NOT NULL,
+    username2 VARCHAR(16) NOT NULL,
+    started_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (username1, username2),
+    FOREIGN KEY (username1) REFERENCES users(username) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (username2) REFERENCES users(username) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    chat_id INT NOT NULL,
+    sender VARCHAR(16) NOT NULL,
+    content VARCHAR(1000) NOT NULL CHECK (content <> ''),
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (chat_id) REFERENCES chats(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (sender) REFERENCES users(username) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE friends (
+    username VARCHAR(16) NOT NULL,
+    friend VARCHAR(16) NOT NULL,
+    status ENUM('pending', 'accepted', 'blocked') NOT NULL,
+    PRIMARY KEY(username, friend),
+    FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (friend) REFERENCES users(username) ON DELETE CASCADE ON UPDATE CASCADE
+);
